70cecc12a93aeaaa5545f7431f0005e2b8f4014a
Eric Blomquist <ericbl@kth.se>
2015-07-26 14:23:58 +0200

    added description and yum button

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express                 = require('express'),
+	mongoose				= require('mongoose'),
     app                     = express(),
     http                    = require('http').Server(app);
 
