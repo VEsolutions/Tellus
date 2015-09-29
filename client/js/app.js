b2d82b8d44c00a651fc7a20d77a03b8f6e6dfda1
relmgren <elmgren93@gmail.com>
2015-09-29 22:28:11 +0200

    fixated navbar to bottom

--- client/js/app.js
+++ client/js/app.js
@@ -1,28 +1,4 @@
 (function(){
- /*   var app = angular.module('tellUs', ['ngResource', 'ui.bootstrap']);
-
-    app.factory('ItemService', function($resource) {
-        return $resource('./items/:id', {id: '@id'}, {
-            query: {method:'GET', isArray:true },
-            save: {method:'POST'}
-        });
-    });
-
-
-    app.controller('itemsController', function ($resource, ItemService) {
-        this.items = [];
-
-        this.items = ItemService.query();
-
-        this.addPost = function(ItemService) {
-            this.items.push([this.title, this.description]);
-            ItemService.save([this.title, this.description]);
-        };
-
-
-    });*/
-
-
     var app = angular.module('tellUs', ['ui.bootstrap']);
 
     app.controller('itemsController', ['$http', function($http) {
@@ -36,15 +12,6 @@
         })
 
         itemsCtrl.addPost = function() {
-/*            var request = $http({
-                    headers: {'Content-Type': 'application/json'},
-                    method: 'POST',
-                    url: './items',
-                    data: { name: itemsCtrl.title,
-                            description: itemsCtrl.description},
-                    params: {name: itemsCtrl.title,
-                            description: itemsCtrl.description}
-            })*/
             var data = {name: itemsCtrl.title, description: itemsCtrl.description};
             $http.post('/items', data).then(function(response) {
                 itemsCtrl.items.push({name: itemsCtrl.title, description: itemsCtrl.description, yum: "0"});
